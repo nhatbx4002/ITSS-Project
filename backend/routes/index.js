f2d42754b3b946629fd72c1a5e42944e930d6a2c
nhatbx4002 <nhatbui2004.work@gmail.com>
2025-04-30 17:55:07 +0700

    add basic trainer-services and trainer-controllers

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,9 +4,10 @@ const router = express.Router();
 // Import all route files
 const productRoutes = require('./productRoutes');
 const authRoutes = require('./authRoutes');
-
+const trainerRoutes = require('./trainerRoutes');
 // Use routes
 router.use('/products', productRoutes);
 router.use('/auth', authRoutes);
+router.use('/trainer' , trainerRoutes);
 
 module.exports = router; 
