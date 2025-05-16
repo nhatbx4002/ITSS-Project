94a2261df928ffad237411838e94f1362df53840
HuyLong2k4 <maihuylong102@gmail.com>
2025-05-17 02:26:50 +0700

    doneMembership

--- backend/controllers/membershipController.js
+++ backend/controllers/membershipController.js
@@ -5,9 +5,9 @@ const membershipController = {
     updateMembership: async (req, res) => {
         try {
             const id = req.params.id
-            const { price } = req.body;
+            const { name, duration, price, type } = req.body;
 
-            const updatedMembership = await membershipService.updateMembership(id, price);
+            const updatedMembership = await membershipService.updateMembership(id, name, duration, price, type);
 
             res.json({
                 success: true,
@@ -38,6 +38,57 @@ const membershipController = {
             })
         }
     },
+
+    createMembership: async (req, res) => {
+        try {
+            const { name, duration, price, type } = req.body;
+
+            const newMembership = await membershipService.createMembership({
+                name,
+                duration,
+                price,
+                type,
+            });
+
+            res.status(201).json({
+                success: true,
+                data: newMembership
+            });
+        } catch(error) {
+            console.error('Chi tiết lỗi khi tạo membership:', error);
+            res.status(500).json({
+                success: false,
+                message: "Lỗi tạo membership"
+            })
+        }
+    },
+
+    deleteMembership: async (req, res) => {
+        try {
+            const id = req.params.id;
+            const deletedMembership = await membershipService.deleteMembership(id);
+
+            if (!deletedMembership) {
+                return res.status(404).json({
+                    success: false,
+                    message: 'Membership not found'
+                });
+            }
+
+            res.json({
+                success: true,
+                message: 'Membership deleted successfully',
+                data: deletedMembership
+            });
+        } catch(error) {
+            console.error('Error updating Membership:', error);
+            res.status(500).json({
+                success: false,
+                message: 'Lỗi server khi cập nhật Membership',
+                error: process.env.NODE_ENV === 'development' ? error.message : undefined
+            })
+        }
+    }
 
 }
 
