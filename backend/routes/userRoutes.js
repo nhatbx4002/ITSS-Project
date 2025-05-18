f76db1aa416d5842128cb232760de9da0a181e21
HuyLong2k4 <maihuylong102@gmail.com>
2025-05-18 17:31:17 +0700

    memberApi

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,11 +8,16 @@ router.post('/', userController.createUser);
 // Lấy tất cả user
 router.get('/', userController.getAllUsers);
 
+router.get('/member', userController.getAllMember);
+
+router.put('/member', userController.updateMember);
 // Lấy user theo ID
 router.get('/:id', userController.getUserById);
 
 router.put('/:id', userController.updateUser);
 
 router.delete('/:id', userController.deleteUser);
+
+
 
 module.exports = router;
